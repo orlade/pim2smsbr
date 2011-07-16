6bad0f794004bf2a8b4d1b052e888fb9ba076a37
Oliver Lade <piemaster21@gmail.com>
2011-07-16 18:07:46 +1000

    Multiplied times by 1000 (to ms) to fixed time conversion bug. Fixed case-sensitive string comparisons. Removed occasional 'b' from file operations. Consistentified quote usage.

--- src/pim2smsbr.py
+++ src/pim2smsbr.py
@@ -14,17 +14,19 @@ from xml.sax.saxutils import escape
 
 # NOTE: The PIM Backup file must first be manually converted to "UTF-8 without BOM" encoding
 
+SMS_LABEL = "IPM.SMStext"
+
 # Create a CSV reader for the source file
 def get_reader(source):
     ext = source[-4:]
     # If the message file is given directly, open it
-    if ext == ".csm" or ext == ".csv":
-        csv_file = open(source, "rb")
+    if ext.lower() in ('.csm', '.csv'):
+        csv_file = open(source, 'r')
     # If the PIB file is given, extract the message file and open it
-    elif ext == ".pib":
+    elif ext.lower() == '.pib':
         zip = ZipFile(source, 'r')
         for filename in zip.namelist():
-            if filename[-4:] == ".csm":
+            if filename[-4:].lower() == '.csm':
                 csv_file = zip.open(filename, 'r')
                 break
     else:
@@ -34,19 +36,19 @@ def get_reader(source):
         raise Exception("Couldn't load messages file, please check your input.")
 
     # Read the file contents
-    sms_text = csv_file.read().decode("utf-16").split(os.linesep)
-    sms_reader = csv.reader(sms_text, delimiter=';', quotechar='"', escapechar="\\")
+    sms_text = csv_file.read().decode('utf-16').split(os.linesep)
+    sms_reader = csv.reader(sms_text, delimiter=';', quotechar='"', escapechar='\\')
     # Return the reader and the eventual source filename
     return sms_reader
 
 # Convert the PIM file to XML
 def convert(source, out):
-    out_str = ""
-    print "Reading input from " + source + "..."
+    out_str = ''
+    print "Reading input from %s..." % source
     try:
         sms_reader = get_reader(source)
     except IOError:
-        print "Input file not found at " + source + ", aborting"
+        print "Input file not found at %s, aborting" % source
         return
 
     print "Processing SMS messages..."
@@ -57,10 +59,10 @@ def convert(source, out):
             continue
         msg_class = row[10]
         # If the entry is an SMS
-        if msg_class == "IPM.SMStext":
+        if msg_class.lower() == SMS_LABEL.lower():
             # Process it
             msg_xml = process(row)
-            out_str += "\t" + msg_xml + "\n"
+            out_str += '\t' + msg_xml + '\n'
             sms_count += 1
     print "Processing %d messages complete!" % sms_count
 
@@ -70,8 +72,8 @@ def convert(source, out):
 
     # Write the output
     print "Writing output to " + out
-    out_file = open(out, "wb")
-    out_file.write("<?xml version='1.0' encoding='UTF-8' standalone='yes' ?>\n")
+    out_file = open(out, 'w')
+    out_file.write('<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>\n')
     out_file.write('<smses count="%d">\n%s</smses>' % (sms_count, out_str))
     print "Output written!"
 
@@ -79,40 +81,40 @@ def convert(source, out):
 def process(row):
     subject = row[4]
     body = escape(row[5]).replace('"', '&quot;')
-    date = 0;
-    if row[16] != "":
-        date = int(time.mktime(time.strptime(row[16], "%Y,%m,%d,%H,%M,%S")))
+    date = 0
+
+    if row[16] != '':
+        # Date should be timestamp in ms
+        date = int(time.mktime(time.strptime(row[16], '%Y,%m,%d,%H,%M,%S'))) * 1000
 
     # If the SMS was received (sender not empty)
     if row[2]:
         # Match a string of digits with an optional plus
-        match = re.search("[^+\d]*(\+*[\d]+)\D*", row[2])
+        match = re.search('[^+\d]*(\+*[\d]+)\D*', row[2])
         address = match.group(1) if match else None
         type = 1
-        svc_ctr = "+491722270333"
         status = -1
 
     # Else the message was sent
     else:
         address = row[18].split(';')[2].strip('\\')
         type = 2
-        svc_ctr = "null"
         status = 0
 
     # Generate the XML node
     msg_xml = '<sms protocol="%s" address="%s" date="%s" type="%s" subject="%s" body="%s" toa="%s" sc_toa="%s" service_center="%s" read="%s" status="%s" locked="%s" />' % \
-    (0, address, date, type, subject, body, "null", "null", svc_ctr, 1, status, 0)
+    (0, address, date, type, subject, body, 'null', 'null', 'null', 1, status, 0)
 
     return msg_xml
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     import argparse
 
-    parser = argparse.ArgumentParser(description='Convert a PIM Backup messages file to SMS Backup & Restore-compatible XML.')
+    parser = argparse.ArgumentParser(description="Convert a PIM Backup messages file to SMS Backup & Restore-compatible XML.")
     parser.add_argument('source', metavar='source_file', type=str, nargs=1,
-                       help='the source file to convert')
+                       help="the source file to convert")
     parser.add_argument('out', metavar='out_file', type=str, nargs='?',
-                       help='optionally specify a file to write the output to')
+                       help="optionally specify a file to write the output to")
     args = parser.parse_args()
 
     # Convert the given input file
